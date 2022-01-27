c853c1fcd74d4c3f4a74eb570d38af4fdf16a79f
Sean Harrison <sean.harrison@inmar.com>
2022-01-27 16:23:40 -0500

    5-methods-22 Readers - solution 1

--- 5-methods-22.go
+++ 5-methods-22.go
@@ -10,7 +10,11 @@ import "golang.org/x/tour/reader"
 
 type MyReader struct{}
 
-// TODO: Add a Read([]byte) (int, error) method to MyReader.
+// Add a Read([]byte) (int, error) method to MyReader.
+func (r MyReader) Read(b []byte) (int, error) {
+	b[0] = 'A'
+	return 1, nil
+}
 
 func main() {
 	reader.Validate(MyReader{})

002a118077ed7f92d6a808c792ed85dba75e4b38
Sean Harrison <sean.harrison@inmar.com>
2022-01-27 15:59:29 -0500

    4-moretypes-18 Slices - solution 1

--- 4-moretypes-18.go
+++ 4-moretypes-18.go
@@ -18,6 +18,17 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	// allocate y
+	y := make([][]uint8, dy)
+	for i := range y {
+		y[i] = make([]uint8, dx)
+		// draw the picture
+		for j := range y[i] {
+			y[i][j] = uint8((i + j) / 2)
+		}
+	}
+
+	return y
 }
 
 func main() {

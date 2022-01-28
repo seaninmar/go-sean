7433ce6c9a8b90017566069650defecd9333bfe4
Sean Harrison <sean.harrison@inmar.com>
2022-01-28 12:41:45 -0500

    move go-tour files to go-tour subfolder

--- go-tour/6-concurrency-10.go
+++ go-tour/6-concurrency-10.go
@@ -18,8 +18,7 @@ import (
 )
 
 type Fetcher interface {
-	// Fetch returns the body of URL and
-	// a slice of URLs found on that page.
+	// Fetch returns the body of URL and a slice of URLs found on that page.
 	Fetch(url string) (body string, urls []string, err error)
 }
 

8ca7f52ee3ea88066495aaeba72c8d0bc6557454
Sean Harrison <sean.harrison@inmar.com>
2022-01-28 11:39:37 -0500

    4-moretypes-23 Maps - implemented WordCount

--- 4-moretypes-23.go
+++ 4-moretypes-23.go
@@ -12,10 +12,15 @@ package main
 
 import (
 	"golang.org/x/tour/wc"
+	"strings"
 )
 
 func WordCount(s string) map[string]int {
-	return map[string]int{"x": 1}
+	m := make(map[string]int)
+	for _, val := range strings.Fields(s) {
+		m[val] += 1
+	}
+	return m
 }
 
 func main() {
